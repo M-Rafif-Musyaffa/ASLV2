734dd01e7d0aaad0cb5157c772535cf2e92788a1
M Rafif Musyaffa <88952324+M-Rafif-Musyaffa@users.noreply.github.com>
2024-11-03 00:42:19 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function startGame() {
+    document.getElementById('output').innerHTML = "<p>Mini-Game started! (This is just a placeholder.)</p>";
+}
+
+function simulateASL() {
+    document.getElementById('output').innerHTML = "<p>ASL Simulation started! (This is just a placeholder.)</p>";
+}
